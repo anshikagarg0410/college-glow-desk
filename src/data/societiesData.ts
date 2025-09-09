b58584f95e9c9fad135388db1923a66f4643d3ca
SafeGuard AI Deploy <deploy@safeguard-ai.com>
2025-09-09 17:53:18 +0530

    updations

--- src/data/societiesData.ts
+++ src/data/societiesData.ts
@@ -13,44 +13,45 @@
 //     category: "Technical Societies",
 //     societies: [
 //       {
-//         name: "GDSC",
-//         description: "GDSC is a Google Developers student club that helps students bridge the gap between theory and practice and build their skills in the field of technology.",
+//         name: "GDSC IGDTUW",
+//         description: "GDSC IGDTUW is a student community focused on Google technologies. They offer workshops and projects in areas like Android, Flutter, and Cloud Computing to help students solve real-world problems",
 //       },
 //       {
 //         name: "MSC",
-//         description: "MSC is a Microsoft Student Community that aims to empower students to build, innovate, and connect with the latest Microsoft technologies.",
+//         description: "MSC provides guidance for students' engineering journeys. They offer sessions and events where students can learn from the experiences of experts, seniors, and peers",
+        
 //       },
 //       {
 //         name: "IEEE Student Chapter",
-//         description: "The IEEE Student Chapter provides a platform for students to explore their interests in various technical fields and gain professional experience.",
+//         description: "A platform for women engineers. The society encourages technical excellence and innovation through various events, competitions, and conferences, with the goal of contributing to a globally.",
 //       },
 //       {
 //         name: "Techneeds",
-//         description: "Techneeds focuses on providing technical skills and knowledge to students through workshops and events.",
+//         description: "It is a community for tech enthusiasts .The organization hosts hackathons to promote innovation and speaker sessions with industry experts to share knowledge and experiences.",
 //       },
 //       {
 //         name: "Assetmarkle",
-//         description: "Assetmarkle is a technical society that provides a platform for students to learn and work on real-world projects in the field of technology.",
+//         description: "AssetMerkle isn't just a society—it's a family dedicated to helping students navigate the complex world of blockchain and Web3.",
 //       },
 //       {
 //         name: "Optica",
-//         description: "Optica is a society dedicated to the science and applications of light, focusing on optics and photonics.",
+//         description: "It aims to enrich the typical technical college experience by providing opportunities to broaden your knowledge and perspectives.",
 //       },
 //       {
 //         name: "Lean In",
-//         description: "Lean In is a community that encourages women to achieve their ambitions and provides a platform for professional growth and support.",
+//         description: "Lean In offers a range of events throughout the year, including an annual hackathon that attracts over 800 coders",
 //       },
 //       {
 //         name: "AWS",
-//         description: "The AWS Student Club aims to help students learn and work on Amazon Web Services technologies.",
+//         description: "dedicated to fostering a vibrant, student-driven community focused on the exploration and application of Amazon Web Services (AWS).",
 //       },
 //       {
 //         name: "HackClub",
-//         description: "HackClub is a global nonprofit network of student-led coding clubs where students build things and learn to code.",
+//         description: "To cultivate an inspiring and dynamic community where tech enthusiasts of all levels can come together to learn, innovate, collaborate and grow.",
 //       },
 //       {
 //         name: "Nirvana",
-//         description: "Nirvana is a technical society focused on various fields of engineering and technology.",
+//         description: "The Design Club of IGDTUW, our motto is Design. Innovate. Inspire We envision being the leading platform for design innovation",
 //       },
 //       {
 //         name: "AI CLUB",
@@ -138,6 +139,7 @@
 interface Society {
   name: string;
   description: string;
+  website: string;
 }
 
 interface SocietyCategory {
@@ -150,60 +152,74 @@ export const societiesData: SocietyCategory[] = [
     category: "Technical Societies",
     societies: [
       {
-        name: "GDSC",
-        description: "GDSC is a Google Developers student club that helps students bridge the gap between theory and practice and build their skills in the field of technology.",
+        name: "GDSC IGDTUW",
+        description: "GDSC IGDTUW is a student community focused on Google technologies. They offer workshops and projects in areas like Android, Flutter, and Cloud Computing to help students solve real-world problems",
+        website: "https://www.instagram.com/gdsc_igdtuw/",
       },
       {
         name: "MSC",
-        description: "MSC is a Microsoft Student Community that aims to empower students to build, innovate, and connect with the latest Microsoft technologies.",
+        description: "MSC provides guidance for students' engineering journeys. They offer sessions and events where students can learn from the experiences of experts, seniors, and peers",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "IEEE Student Chapter",
-        description: "The IEEE Student Chapter provides a platform for students to explore their interests in various technical fields and gain professional experience.",
+        description: "A platform for women engineers. The society encourages technical excellence and innovation through various events, competitions, and conferences, with the goal of contributing to a globally.",
+        website: "https://ieee-igdtuw.github.io/IEEE-IGDTUW-Official-Website/",
       },
       {
         name: "Techneeds",
-        description: "Techneeds focuses on providing technical skills and knowledge to students through workshops and events.",
+        description: "It is a community for tech enthusiasts .The organization hosts hackathons to promote innovation and speaker sessions with industry experts to share knowledge and experiences.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "Assetmarkle",
-        description: "Assetmarkle is a technical society that provides a platform for students to learn and work on real-world projects in the field of technology.",
+        description: "AssetMerkle isn't just a society—it's a family dedicated to helping students navigate the complex world of blockchain and Web3.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "Optica",
-        description: "Optica is a society dedicated to the science and applications of light, focusing on optics and photonics.",
+        description: "It aims to enrich the typical technical college experience by providing opportunities to broaden your knowledge and perspectives.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "Lean In",
-        description: "Lean In is a community that encourages women to achieve their ambitions and provides a platform for professional growth and support.",
+        description: "Lean In offers a range of events throughout the year, including an annual hackathon that attracts over 800 coders",
+        website: "https://leanin.org/circles-network/lean-in--igdtuw",
       },
       {
         name: "AWS",
-        description: "The AWS Student Club aims to help students learn and work on Amazon Web Services technologies.",
+        description: "dedicated to fostering a vibrant, student-driven community focused on the exploration and application of Amazon Web Services (AWS).",
+        website: "https://www.meetup.com/ko-KR/aws-cloud-club-igdtuw/",
       },
       {
         name: "HackClub",
-        description: "HackClub is a global nonprofit network of student-led coding clubs where students build things and learn to code.",
+        description: "To cultivate an inspiring and dynamic community where tech enthusiasts of all levels can come together to learn, innovate, collaborate and grow.",
+        website: "https://www.hackwithindia.in/Chapters",
       },
       {
         name: "Nirvana",
-        description: "Nirvana is a technical society focused on various fields of engineering and technology.",
+        description: "The Design Club of IGDTUW, our motto is Design. Innovate. Inspire We envision being the leading platform for design innovation",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "AI CLUB",
         description: "The AI Club is a society for students interested in Artificial Intelligence and Machine Learning, providing a platform to learn and build projects.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "CodeBenders",
         description: "CodeBenders is a coding community that aims to improve students' coding skills and prepare them for technical challenges.",
+        website: "https://codebendersigdtuw.github.io/Codebender-22/",
       },
       {
         name: "Minerva (virtual tech forum)",
         description: "Minerva is a virtual tech forum that hosts online discussions and workshops on the latest technologies.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "ROBOLUTION (robotics team)",
         description: "ROBOLUTION is the official robotics team of the college, where students design, build, and program robots for various competitions.",
+        website: "https://robolution.in/",
       },
     ],
   },
@@ -213,18 +229,22 @@ export const societiesData: SocietyCategory[] = [
       {
         name: "Tarannum (music society)",
         description: "Tarannum is the music society of IGDTUW, dedicated to promoting and celebrating music in all its forms.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "ZENA (Fashion Society)",
         description: "ZENA is the fashion society that provides a platform for students to showcase their creativity and passion for fashion.",
+        website: "https://www.instagram.com/zena.igdtuw/",
       },
       {
         name: "RAHNUMA (Dramatics Society)",
         description: "RAHNUMA is the dramatics society that helps students hone their acting and stage performance skills.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "HYPNOTICS (Dance Society)",
         description: "HYPNOTICS is the dance society that promotes and celebrates various dance forms among the students.",
+        website: "https://www.igdtuw.ac.in/",
       },
     ],
   },
@@ -234,14 +254,17 @@ export const societiesData: SocietyCategory[] = [
       {
         name: "B.H.A.V.-The literary and public speaking society of IGDTUW",
         description: "BHAV is the literary and public speaking society that aims to improve communication and writing skills among students.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "Finivesta (finance and economic society)",
         description: "Finivesta is the finance and economic society that helps students understand the world of finance and economics.",
+        website: "https://official-website-finivesta.vercel.app/",
       },
       {
         name: "Technoliterati (The Literary Society)",
         description: "Technoliterati is the literary society that provides a platform for students to express their thoughts through writing and literature.",
+        website: "https://m.youtube.com/@technoliteratiigdtuw3596",
       },
     ],
   },
@@ -251,10 +274,12 @@ export const societiesData: SocietyCategory[] = [
       {
         name: "Soch",
         description: "Soch is the media and creative society that covers and documents various events and activities of the college.",
+        website: "https://www.igdtuw.ac.in/",
       },
       {
         name: "PREKSHYA",
         description: "PREKSHYA is a creative society dedicated to art, photography, and other creative pursuits.",
+        website: "https://www.igdtuw.ac.in/",
       },
     ],
   },
@@ -264,10 +289,12 @@ export const societiesData: SocietyCategory[] = [
       {
         name: "Protege (mentorship society)",
         description: "Protege is a mentorship society that connects students with mentors to guide them in their academic and career paths.",
+        website: "https://protege-mentoring.com/",
       },
       {
         name: "INSTINCT (The Peer Educator's and Life Skills society)",
         description: "INSTINCT is a society that focuses on developing life skills and providing peer education to the students.",
+        website: "https://www.igdtuw.ac.in/",
       },
     ],
   },
